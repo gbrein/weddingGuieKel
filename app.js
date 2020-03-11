0e6b1d880be7d3edaaaa6b737d35f04b6322ee3f
gbrein1 <guilherme.brein@siteblindado.com.br>
2020-03-11 16:49:57 -0300

    ajuste fino MELI

--- app.js
+++ app.js
@@ -50,6 +50,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 // default value for title local
 app.locals.title = 'Express ';
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "casamentoguiekel.com.br"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  // res.header('Access-Control-Allow-Methods', 'GET,POST');
+  next();
+});
+
 const index = require('./routes/index');
 app.use('/', index);
 
